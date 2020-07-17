77b4aa2ce8cd89baf4aab07c92d420d16caff928
Henrique Leite <henriqueleite616@gmail.com>
2020-07-16 21:56:15 -0300

    Updates

---
+++ precache-manifest.749939dfb3bc3033c2ffad77577c71cd.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "552b8856adbf8f6df13c878477147246",
+    "url": "/react-resume-generator/index.html"
+  },
+  {
+    "revision": "9abffcdf623a4eeaaf18",
+    "url": "/react-resume-generator/static/css/main.aa023854.chunk.css"
+  },
+  {
+    "revision": "72cc2c311366c18585ed",
+    "url": "/react-resume-generator/static/js/0.55078f1c.chunk.js"
+  },
+  {
+    "revision": "783f14fa45b10e088e68f98251448010",
+    "url": "/react-resume-generator/static/js/0.55078f1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd585a3fd1b5e4529d66",
+    "url": "/react-resume-generator/static/js/1.9f0ecd66.chunk.js"
+  },
+  {
+    "revision": "81c1c3db7a295dd59e70",
+    "url": "/react-resume-generator/static/js/2.276f3574.chunk.js"
+  },
+  {
+    "revision": "6e04fd3ec731dc7543c3",
+    "url": "/react-resume-generator/static/js/5.cd577eac.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-resume-generator/static/js/5.cd577eac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9abffcdf623a4eeaaf18",
+    "url": "/react-resume-generator/static/js/main.194bfb13.chunk.js"
+  },
+  {
+    "revision": "e577b47784b56b328392",
+    "url": "/react-resume-generator/static/js/runtime-main.4faee494.js"
+  },
+  {
+    "revision": "ade91f473255991f410f61857696434b",
+    "url": "/react-resume-generator/static/media/Montserrat-Bold.ade91f47.ttf"
+  },
+  {
+    "revision": "1b38414956c666bd1df78fe5b9c84756",
+    "url": "/react-resume-generator/static/media/Montserrat-BoldItalic.1b384149.ttf"
+  },
+  {
+    "revision": "a7063e0c0f0cb546ad45e9e24b27bd3b",
+    "url": "/react-resume-generator/static/media/Montserrat-Italic.a7063e0c.ttf"
+  },
+  {
+    "revision": "c8b6e083af3f94009801989c3739425e",
+    "url": "/react-resume-generator/static/media/Montserrat-Medium.c8b6e083.ttf"
+  }
+]);
